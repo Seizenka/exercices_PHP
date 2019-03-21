37db84bc1f4516f158b7520556160bc97d76260a
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-21 13:58:03 +0100

    ajout-exercice 4

---
+++ les_variables/04/index.php
@@ -0,0 +1,23 @@
+<?php
+$var_int;
+var_dump($var_int);
+echo $var_int; echo "<p></p>";
+$var_int = 2;
+echo $var_int;
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les variables - Exercice 4</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
