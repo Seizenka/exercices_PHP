7099021aaf835bf922fbd68f75d11989222cf7ea
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-21 14:51:38 +0100

    ajout-exercice 6

---
+++ les_variables/06/index.php
@@ -0,0 +1,27 @@
+<?php
+echo "<h1>Les variables - Exercice 6</h1>";
+$prix = 785;
+$ristourne = 30;
+$somme = ($prix * $ristourne) / 100;
+echo $somme;
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les variables - Exercice 6</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
