0f1e89129ff95df78c17b4c8ccda0707f8545fe0
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-21 12:18:02 +0100

    ajout - exercice 3

---
+++ les_variables/03/index.php
@@ -0,0 +1,30 @@
+<?php
+echo "<h1>Les variables - Exercice 3</h1>";
+$myString = 'Christine and the queens';
+echo $myString; echo "<p></p>";
+$myInteger = 2019;
+echo $myInteger; echo "<p></p>";
+$myFloat = 1.7;
+echo "Ceci est un float : $myFloat"; echo "<p></p>";
+$myBoolean = true; echo "<p></p>";
+if($myBoolean == true)
+  echo "TRUE";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les Variables - Exercice 3</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
