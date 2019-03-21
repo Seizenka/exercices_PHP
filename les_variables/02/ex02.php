b3a8c8efbebbd12235569afabe204832ce84bc8a
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-21 11:29:18 +0100

    ajout-exercice-02

---
+++ les_variables/02/ex02.php
@@ -0,0 +1,25 @@
+<?php
+ echo "<h1>Les variables - Exercice 2</h1>";
+$km = 1;
+echo "Le premier affiche $km ";
+$km = 3;
+echo "le deuxième affiche $km ";
+$km = 125;
+echo "le troisième affiche $km ";
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les variables - Exercice 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
