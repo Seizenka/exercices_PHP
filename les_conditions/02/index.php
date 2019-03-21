6b66bae9f3b78b3d3cfa1ebd2e139bea38998c96
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-21 15:52:19 +0100

    ajout-exercice 2

---
+++ les_conditions/02/index.php
@@ -0,0 +1,25 @@
+<?php
+$isEasy = true;
+if($isEasy == true) {
+    echo 'C\'est facile !';
+} else {
+    echo 'C\'est difficile !';
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les conditions - Exercice 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
