0f7a6b78305b0482ecae006d3cbbdf6525af632d
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-21 14:06:34 +0100

    ajout-exercice 5

---
+++ les_variables/05/index.php
@@ -0,0 +1,29 @@
+<?php
+echo "<h1>Les variables - Exercice 5</h1>";
+
+$result1 = 3 + 4;
+echo $result1; echo "<p></p>";
+$result2 = 5 * 20;
+echo $result2; echo "<p></p>";
+$result3 = 45 / 5;
+echo $result3;
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les variables - Exercice 5</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
