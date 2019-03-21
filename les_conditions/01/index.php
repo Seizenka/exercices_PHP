6ee2d8247364a5c77db87997c5e67426228025be
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-21 15:41:08 +0100

    ajout-les conditions

---
+++ les_conditions/01/index.php
@@ -0,0 +1,26 @@
+<?php
+$age = 18;
+
+if($age >= 18) {
+    echo 'Vous êtes majeur.<p></p>';
+} else {
+    echo 'Vous êtes mineur !<p></p>';
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les conditions - Exercice 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
