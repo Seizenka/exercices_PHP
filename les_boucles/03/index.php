d6037e1dfc4303191b269a215b226b1a1ec421be
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-27 22:29:43 +0100

    ajout-exercice 3

---
+++ les_boucles/03/index.php
@@ -0,0 +1,26 @@
+<?php
+    echo "<h1>PHP le boucles - Exercie 3</h1>";
+    $nbr = 100;
+    $nbr2 = 56;
+
+ do{
+     $result = $nbr * $nbr2;
+     echo $result. "";
+     $nbr++;
+ } while($nbr >= 10);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les boucles - Exercice 3</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
