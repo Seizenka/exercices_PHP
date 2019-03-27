d7fad815eb3a798ba19fe7af11e560a3cee233ab
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-27 22:16:19 +0100

    ajout-exercice-1

---
+++ les_boucles/01/index.php
@@ -0,0 +1,24 @@
+<?php
+    $i = 0;
+
+    while($i <= 10){
+        echo($i . '');
+        $i++;
+    }
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les boucles - Exercice 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
