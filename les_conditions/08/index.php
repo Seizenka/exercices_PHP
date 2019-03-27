0efa6eeab03e2784b7b4e964fe3c318bb93eb6a2
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-27 22:10:31 +0100

    ajout-exercice-8

---
+++ les_conditions/08/index.php
@@ -0,0 +1,28 @@
+<?php
+echo "<h1>Les conditions - Exercice 8</h1>";
+/* <?php
+  echo ($maVariable) ? 'c\'est ok !!' : 'c\'est pas bon !!!';
+?>
+ */
+    if($maVariable){
+        echo "C'est ok";
+    } else{
+        echo "C'est pas bon !";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les conditions - Exercice 8</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+   
+</body>
+</html>
