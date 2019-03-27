bb4999d1c76615843a374a0db1ee530164d7efe5
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-27 22:50:18 +0100

    ajout-exercice-7

---
+++ les_boucles/07/index.php
@@ -0,0 +1,26 @@
+<?php
+echo "<h1>PHP le boucles - Exercie 7</h1>";
+
+    $pas = 1;
+
+    do{
+        echo "On tient le bon bout<p></p>";
+        $pas += 15;
+    } while($pas < 100);
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les boucles - Exercice 7</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
