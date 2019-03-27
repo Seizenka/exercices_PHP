eed94b75341c6dcebc90787f997a2fbd0e76ff95
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-27 22:46:29 +0100

    ajout-exercice-6

---
+++ les_boucles/06/index.php
@@ -0,0 +1,27 @@
+<?php
+echo "<h1>PHP le boucles - Exercie 6</h1>";
+
+$pas = 20;
+
+do{
+    echo "C'est presque bon<p></p>";
+    $pas--;
+
+} while($pas > 0);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les boucles - Exercice 6</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
