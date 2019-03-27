28ada1757cdb2cad19c071b8df6bb9421f3dcddc
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-28 00:13:13 +0100

    ajout-exercice-4

---
+++ les_tableaux/04/index.php
@@ -0,0 +1,25 @@
+<?php
+    echo "<h1>Les tableaux - Exercice 4</h1>";
+
+    $contact = ["Alex", "Max", "Dominique", "Claude", "Leslie", "Charlie", "Lou"];
+    foreach($contact as $value){
+        echo "<p>"."Salut ".$value.", devine quoi ! Je me marie dans samedi dans deux semaines ! J'espère te compter parmi les invités ! Gros bisous :)"."</p>";
+        
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les tableaux - Exercice 4</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
