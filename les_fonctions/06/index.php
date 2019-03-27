d1a6d8d1878e217a2f4cb5c0d8c766498f8e15b5
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-27 23:34:07 +0100

    ajout-exercice-6

---
+++ les_fonctions/06/index.php
@@ -0,0 +1,23 @@
+<?php
+    echo "<h1>PHP le fonctions - Exercie 6</h1>";
+
+    function concat($nom, $prenom, $age){
+        echo ("Bonjour" + $nom + $prenom + ", tu as " + $age + " ans.");
+    }
+    concat("Caestecker", "Cassandra", 21);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les fonctions - Exercice 6</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
