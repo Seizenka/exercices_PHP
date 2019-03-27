8f8acd67da53fd79ebfc3a4a9c521cd436dce896
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-27 23:00:10 +0100

    ajout-exercice 1

---
+++ les_fonctions/01/index.php
@@ -0,0 +1,24 @@
+<?php
+echo "<h1>PHP le fonctions - Exercie 1</h1>";
+
+    function boolean(){
+        return true;
+    }
+    boolean();
+    echo boolean();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les fonctions - Exercie 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
