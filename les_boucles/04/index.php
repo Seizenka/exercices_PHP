e58661936328eb538c177009e17ead9d7bf02919
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-27 22:37:42 +0100

    ajout-exercice-4

---
+++ les_boucles/04/index.php
@@ -0,0 +1,23 @@
+<?php
+    $nbr1 = 1;
+
+    do{
+        echo $nbr1. "";
+        $nbr1 +=$nbr1/2;
+    } while($nbr1 < 10);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les boucles - Exercice 4</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
