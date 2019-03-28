7d1b92f9a1c8a60b9b5d5d4a7c6f070b8205abbb
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-03-28 09:23:26 +0100

    ajout-exercice-4

---
+++ les_formulaires/04/user.php
@@ -0,0 +1,25 @@
+<?php
+echo "<h1>PHP - Les formulaires - Exercice 4.</h1>";
+$nom = $_POST["nom"];
+$prenom = $_POST['prenom'];
+echo $nom." ".$prenom; 
+    
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP - Les formulaires - Exercice 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+<form action="user.php" method="POST">
+    Votre nom ? <input name="nom" size=25 type="text"/><p></p>
+    Votre prénom ? <input name="prenom" size=25 type="text"/><p></p>
+    <input type=submit value="envoyer" name="envoyer"/>
+</body>
+</html>
